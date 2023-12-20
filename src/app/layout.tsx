ed8ab8b07382ef7cc562fcd662059084806b2ef5
张文皓 <zhangwh754@163.com>
2023-12-20 23:13:11 +0800

    remove: 暂时注释用不到的footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,17 @@ import SiteFooter from '@/components/common/SiteFooter'
 
 export const metadata: Metadata = {
   title: 'Blog of djdg626',
-  description: 'djdg626博客,IT,技术,生活,日常分享,教程,前端,JavaScript',
+  description: '博客,IT,技术,生活,日常分享,教程,前端,JavaScript',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="zh-CN">
       <body>
-        <div className='flex flex-col min-h-screen'>
+        <div className="relative pt-12 flex flex-col min-h-screen">
           <SiteHeader />
-          <main className='flex-grow'>{children}</main>
-          <SiteFooter />
+          <main className="flex-grow">{children}</main>
+          {/* <SiteFooter /> */}
         </div>
       </body>
     </html>
