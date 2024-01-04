ede7decddc7ee853a3b2ed564e8aaca80c41c5cd
张文皓 <zhangwh754@163.com>
2024-01-04 17:24:51 +0800

    feat: 移动端菜单

--- src/components/common/SiteHeader/index.tsx
+++ src/components/common/SiteHeader/index.tsx
@@ -25,7 +25,7 @@ function HeaderLeft() {
   )
 }
 
-function HeaderRight() {
+function HeaderRight(props: { display: Boolean; toggleMenuShow: Function }) {
   return (
     <>
       <nav className="hidden ml-auto text-base font-medium sm:block">
@@ -35,6 +35,21 @@ function HeaderRight() {
           </Link>
         ))}
       </nav>
+
+      {props.display && (
+        <div className="fixed top-0 left-0 z-10 h-full w-full transform bg-gray-200 opacity-95 duration-300 ease-in-out dark:bg-gray-800 translate-x-0">
+          <div className="h-[5.5rem] px-4 text-white flex justify-end items-center">
+            <ToggleMenuShow display={true} toggleMenuShow={() => props.toggleMenuShow()} />
+          </div>
+          <nav className="flex flex-col items-start px-8 text-2xl font-bold">
+            {headerNavLinks.map(link => (
+              <Link key={link.href} className="p-4" href={link.href} onClick={() => props.toggleMenuShow(false)}>
+                {link.title}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      )}
     </>
   )
 }
@@ -90,7 +105,29 @@ function ToggleTheme() {
   )
 }
 
-function ToggleMenuShow() {
+function ToggleMenuShow(props: { display: Boolean; toggleMenuShow: Function }) {
+  const toggleMenuShow = () => {
+    props.toggleMenuShow()
+  }
+
+  return (
+    <>
+      <button
+        className="sm:hidden ml-1 sm:ml-4 p-1 rounded-md w-8 h-8 flex justify-center items-center text-slate-900 dark:text-slate-100"
+        onClick={() => toggleMenuShow()}
+      >
+        <span className="sr-only">Toggle Menu Show</span>
+        {!props.display ? (
+          <Icon name="menu" className={`w-4 h-4`}></Icon>
+        ) : (
+          <Icon name="close" className={`w-4 h-4`}></Icon>
+        )}
+      </button>
+    </>
+  )
+}
+
+export default function Header() {
   const [display, setDisplay] = useState(false)
 
   const toggleMenuShow = (display: Boolean) => {
@@ -105,26 +142,12 @@ function ToggleMenuShow() {
 
   return (
     <>
-      <button
-        className="sm:hidden ml-1 sm:ml-4 p-1 rounded-md w-8 h-8 flex justify-center items-center dark:text-gray-100"
-        onClick={() => toggleMenuShow(!display)}
-      >
-        <span className="sr-only">Toggle Menu Show</span>
-        {!display ? <Icon name="menu" className={`w-4 h-4`}></Icon> : <Icon name="close" className={`w-4 h-4`}></Icon>}
-      </button>
-    </>
-  )
-}
-
-export default function Header() {
-  return (
-    <>
       <header className="fixed top-0 left-0 right-0 flex mx-auto max-w-5xl h-[5.5rem] bg-inherit">
         <div className="px-4 flex-1 flex justify-between items-center">
           <HeaderLeft />
-          <HeaderRight />
+          <HeaderRight display={display} toggleMenuShow={() => toggleMenuShow(false)} />
           <ToggleTheme />
-          <ToggleMenuShow />
+          <ToggleMenuShow display={false} toggleMenuShow={() => toggleMenuShow(true)} />
         </div>
       </header>
     </>
