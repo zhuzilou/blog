a37bb28465732d345e72169b9e64ab6fd65de7a8
张文皓 <zhangwh754@163.com>
2024-01-17 15:34:15 +0800

    fix: fix image url load wrong.

--- src/layout/MdxLayout/ImgBlock.tsx
+++ src/layout/MdxLayout/ImgBlock.tsx
@@ -3,26 +3,26 @@
 import Image from 'next/image'
 
 const imageLoader = ({ src, width, quality }: any) => {
-  console.log('wrong src', src)
+  const url = new URL(`${src}`)
 
-  return `${src}`
+  console.log('url.href', url.href)
+
+  return url.href
 }
 
 export default function ImgBlock(props: any) {
   return (
-    <Image src={props.src} alt={props.alt} width={300} height={100} />
-    // <Image
-    //   src={props.src}
-    //   alt={props.alt}
-    //   sizes="100vw"
-    //   style={{
-    //     width: '100%',
-    //     height: 'auto',
-    //   }}
-    //   loader={imageLoader}
-    //   width={500}
-    //   height={300}
-    //   unoptimized
-    // />
+    <Image
+      src={props.src}
+      alt={props.alt}
+      loader={imageLoader}
+      sizes="100vw"
+      style={{
+        width: '100%',
+        height: 'auto',
+      }}
+      width={500}
+      height={300}
+    />
   )
 }
