0a242c02f1327fd17dce8f2306bb9f056b2b3f5b
张文皓 <zhangwh754@163.com>
2024-04-12 16:34:07 +0800

    feat: upload commit image

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ export default function SiteAbout() {
   return (
     <div className="mx-auto mt-8 max-w-5xl py-5 sm:p-5 bg-base-100 rounded-lg">
       <h2 className="text-4xl text-orange-500">About</h2>
+      <img src="https://ghchart.rshah.org/zhangwh754" alt="zhangwh754's Github chart" />
     </div>
   )
 }
