1b59bdecdd3afef7225ea5118bebedcef7fc6398
youwei.zhang <dx_zhangyw@dxinfor.com>
2025-05-07 19:40:04 +0800

    first try: ThanksTo

--- src/layout/FooterLayout/index.tsx
+++ src/layout/FooterLayout/index.tsx
@@ -1,8 +1,10 @@
+import ThanksTo from "@/components/ThanksTo"
 export default function Footer() {
   return (
     <footer className="absolute left-0 right-0 footer footer-center p-4 bg-base-300 text-base-content">
       <aside>
-        <p>Copyright © 2024 - All right reserved by djdg626</p>
+        <p>Copyright © 2024 - All right reserved by DSL. </p>
+        <ThanksTo className=""/>
       </aside>
     </footer>
   )
