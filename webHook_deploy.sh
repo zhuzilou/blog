1c80c10ac6546b6a1f776eb65b7145f195287621
zyw <zyw0229@163.com>
2025-05-22 11:32:07 +0800

    fix/修改复制错误，通过docker run 添加同步时间方法。与Dockerfile二选一

--- webHook_deploy.sh
+++ webHook_deploy.sh
@@ -1,9 +1,3 @@
------------------End deploy-----------------
-[root@hcss-ecs-1080 shell]# ll
-total 4
--rwxr-xr-x 1 root root 536 May 20 15:28 blog_docker.sh
-[root@hcss-ecs-1080 shell]# vim blog_docker.sh 
-[root@hcss-ecs-1080 shell]# cat blog_docker.sh 
 echo '-----------------Begin deploy-----------------'
 echo '<'`date +%Y-%m-%d_%H:%M:%S`'>Receive request from' $1 
 cd ~/git
@@ -18,6 +12,7 @@ echo '<'`date +%Y-%m-%d_%H:%M:%S`'>'Finish Docker build
 docker stop blog
 docker rm blog
 echo '<'`date +%Y-%m-%d_%H:%M:%S`'>'Remove old container
-docker run -d --restart=always -p 3000:3002 --name blog blog:$TAG
+# 用于同步时区 -v /etc/localtime:/etc/localtime:ro，ro表示只读
+docker run -d --restart=always -p 3000:3002 -v /etc/localtime:/etc/localtime:ro --name blog blog:$TAG
 echo '<'`date +%Y-%m-%d_%H:%M:%S`'>'Docker deploy container blog:$TAG finish
 echo '-----------------End deploy-----------------'
